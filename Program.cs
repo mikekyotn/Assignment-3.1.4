b2460bc86b8853d84b9edf9e027b2c1dcb936ab9
mikekyotn <michael.chankij@mssa.newhorizons.com>
2025-07-22 07:40:01 +0900

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Assignment_3._1._4
+{
+    //Contains a function that takes an int array and finds the first
+    //two consecutive values and changes them to 0s
+    //Status: Not complete, working.
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myArray = { 1, 2, 2, 1, 1, 9, 1 };
+            PrintArray(myArray);
+            
+            FindAndChangeConsecutiveValues(myArray);
+
+            PrintArray(myArray);
+            
+        }
+
+
+        static void FindAndChangeConsecutiveValues(int[] array)
+        {
+            int previousValue = array[0];
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (previousValue == array[i])
+                {
+                    array[i - 1] = 0;
+                    array[i] = 0;
+                    break;
+                }
+                else
+                {
+                    previousValue = array[i];
+                }
+            }
+        }
+
+        static void PrintArray(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+            Console.WriteLine();
+        }
+
+    }
+}
